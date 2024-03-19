0ac231843f8d02ec98f0dac78651956716e1f356
SupriyaSP2000 <supriya242810@gmail.com>
2024-03-19 17:30:47 +0530

    commiting after changes

---
+++ Demo.java
@@ -0,0 +1,14 @@
+package NumberSystem;
+
+public class Basic {
+	
+	public static void main(String[] args) {
+		
+		int num;
+		int num1;
+		num = 10+2*5/3%2;
+		num1 = (10+2)*5/3%2;
+		System.out.println(num);
+		System.out.println(num1);
+	}
+}
